9fd436543d4531b038082cd1bb5921ea31eb36c6
Alenka <ms.alenchik@gmail.com>
2020-11-29 14:03:02 +0200

    Fixed mistake

--- 12.1.js
+++ 12.1.js
@@ -1,11 +1,5 @@
-function getKeys(key, key2) {
-    const objKeys = { 1: key, 2: key2}
-    for (let key in objKeys)
-    if (objKeys.hasOwnProperty(key))
-    { console.log(key) }
-        for (let key in objKeys)
-    { console.log(objKeys[key]) }
-
-}
-
-getKeys(20, 30)
+(function (getKeys = { key1: "value1", key2: "value2" }) {
+    for (let key in getKeys) {
+        getKeys.hasOwnProperty(key) ? console.log(`${key} : ${getKeys[key]}`) : null;
+    }
+})();
