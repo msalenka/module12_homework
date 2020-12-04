c4c44d327d9906083bf6491644bff9ea61ca67a8
Alenka <ms.alenchik@gmail.com>
2020-12-04 17:01:24 +0200

    homework 2

--- 12.2.js
+++ 12.2.js
@@ -1,5 +1,20 @@
-(function (stringIn = { 1: "one", 2: "two", 3: "three" }, string = "one") {
-    if (stringIn[string]) { console.log("true") }
-    else { console.log("false") }
+// Задание 2.
+
+// Написать функцию, которая принимает в качестве аргументов строку и объект,
+// а затем проверяет есть ли у переданного объекта свойство с данным именем.Функция должна возвращать true или false.
+
+function isKey(object, string) {
+    if (string in object) {
+        return true
     }
-)();
+    else { return false };
+    
+}
+
+let someObject = { 1: "one", 2: "two", 3: "three" };
+let someString = 1;
+
+let findValue = isKey(someObject, someString);
+console.log(findValue);
+
+
